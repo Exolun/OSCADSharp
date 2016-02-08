a34992bfaaae0fe2feeac9c1f0af76662ea8a8f0
Mike Smith <msmith@affirma.com>
2016-02-07 21:17:43 -0800

    Refactored UnionObject to primarily just use BlockStatementObject for string construction

---
+++ OSCADSharp/OSCADSharp/BlockStatementObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCADSharp
+{
+
+    /// <summary>
+    /// A statement that has multiple child nodes, whose ToString output
+    /// is more or less just an aggregate of the children
+    /// </summary>
+    internal class BlockStatementObject : OSCADObject
+    {
+        private string outerStatement;
+        private IEnumerable<OSCADObject> children;
+
+        internal BlockStatementObject(string outerStatement, IEnumerable<OSCADObject> children)
+        {
+            this.outerStatement = outerStatement;
+            this.children = children;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var child in this.children)
+            {
+                sb.AppendLine(child.ToString());
+            }
+
+            var formatter = new BlockFormatter(this.outerStatement, sb.ToString());
+            return formatter.ToString();
+        }
+    }
+}
--- OSCADSharp/OSCADSharp/Booleans/Union.cs
+++ OSCADSharp/OSCADSharp/Booleans/Union.cs
@@ -9,30 +9,14 @@ namespace OSCADSharp.Booleans
     /// <summary>
     /// A union of child nodes. This is the sum of all children (logical or).    
     /// </summary>
-    internal class Union : OSCADObject
+    internal class Union : BlockStatementObject
     {
-        private IEnumerable<OSCADObject> children;
-
         /// <summary>
         /// Create a union that is the combination of all children
         /// </summary>
         /// <param name="children">OSCADObjects to combine</param>
-        internal Union(IEnumerable<OSCADObject> children)
+        internal Union(IEnumerable<OSCADObject> children) : base("union()", children)
         {
-            this.children = children;
-        }
-
-        public override string ToString()
-        {
-            string unionCommand = "union()";
-            StringBuilder sb = new StringBuilder();
-            foreach (var child in this.children)
-            {
-                sb.AppendLine(child.ToString());
-            }
-
-            var formatter = new BlockFormatter(unionCommand, sb.ToString());
-            return formatter.ToString();
         }
     }
 }
