0d7eaf89e0e6a9dbe32c023ea7566b5242deebaa
Michael L Smith <michael.lee.smith@live.com>
2016-02-10 21:53:07 -0800

    Added 'sizes' class with some constants and an inches to milimeters function

---
+++ OSCADSharp/OSCADSharp/Sizes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCADSharp
+{
+    public class Sizes
+    {
+        public const double OneInch = 25.4;
+        public const double HalfInch = OneInch / 2;
+        public const double QuarterInch = HalfInch / 2;
+        public const double EigthInch = QuarterInch / 2;
+        public const double SixteenthInch = EigthInch / 2;
+
+        public double InchesToMilimeters(double inches)
+        {
+            return inches * 0.03937008;
+        }
+    }
+}
