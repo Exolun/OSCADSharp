052760ecf12bc60aefcca57f5ccd168dfe7ba943
Michael L Smith <michael.lee.smith@live.com>
2016-02-24 21:31:22 -0800

    Added IFileInvoker

---
+++ OSCADSharp/OSCADSharp/Scripting/IFileInvoker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCADSharp.Scripting
+{
+    /// <summary>
+    /// Opens a file
+    /// </summary>
+    public interface IFileInvoker
+    {
+        /// <summary>
+        /// Opens the specified file
+        /// </summary>
+        /// <param name="path">Path to the file to open</param>
+        /// <param name="arguments">Command-line arguments to pass in</param>
+        void Invoke(string path, string arguments = null);
+    }
+}
