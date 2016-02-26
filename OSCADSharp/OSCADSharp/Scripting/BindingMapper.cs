d5ec961cb10d0c0509242c86cac7c913672b3587
Michael Smith <mike@mikelikescoding.com>
2016-02-25 23:52:16 -0800

    Removed Bind method from OSCADObject, added IBindable, Binding.cs, and BindingMapper stubs.

---
+++ OSCADSharp/OSCADSharp/Scripting/BindingMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCADSharp.Scripting
+{
+    internal class BindingMapper
+    {
+        public string OpenSCADFieldName { get; set; }
+        public List<string> BindingOptions { get; set; }
+        public Dictionary<string, Func<object, string>> BindingTransformers { get; set; }
+    }
+}
---
+++ OSCADSharp/OSCADSharp/Scripting/IBindable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCADSharp.Scripting
+{
+    /// <summary>
+    /// An object whose properties can be bound to variables
+    /// </summary>
+    public interface IBindable
+    {
+        /// <summary>
+        /// Binds a variable to property of this object
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="variable"></param>
+        void Bind(string property, Variable variable);
+    }
+}
