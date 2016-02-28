cf189fb075ae5644394a9dd08b6dac9dda44faf9
Michael L Smith <michael.lee.smith@live.com>
2016-02-28 15:26:03 -0800

    Typo fix

--- OSCADSharp/OSCADSharp/Settings/OpenSCADPathFinder.cs
+++ OSCADSharp/OSCADSharp/Settings/OpenSCADPathFinder.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace OSCADSharp
 {
     /// <summary>
-    /// Knowns where the default installation locations for OpenSCAD
+    /// Known where the default installation locations for OpenSCAD
     /// are, and finds the right path for the current environment
     /// </summary>
     internal class OpenSCADPathFinder
