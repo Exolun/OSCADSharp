cdc923c3b5dfd88b7a96a53d7ae6a8023c3f0631
Michael Smith <mike@mikelikescoding.com>
2016-03-06 21:00:24 -0800

    Updated assembly properties, added LICENSE.txt (MIT)

--- OSCADSharp/OSCADSharp/Properties/AssemblyInfo.cs
+++ OSCADSharp/OSCADSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OSCADSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A C# library for generating OpenSCAD Code")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OSCADSharp")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
