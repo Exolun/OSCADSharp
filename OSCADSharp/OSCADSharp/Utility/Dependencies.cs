ddf41907280bd702eb2a239f2dabc0b55aea433c
Michael Smith <mike@mikelikescoding.com>
2016-03-18 00:03:31 -0700

    Moved Dependencies to Utility

--- OSCADSharp/OSCADSharp/Utility/Dependencies.cs
+++ OSCADSharp/OSCADSharp/Utility/Dependencies.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OSCADSharp
+namespace OSCADSharp.Utility
 {
     /// <summary>
     /// Contains definitions for external APIs used by OSCADSharp
