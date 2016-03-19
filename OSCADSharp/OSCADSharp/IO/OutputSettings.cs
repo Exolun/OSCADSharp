ca6bc89206f6c1ce7b21952a9514c76a050daf98
Michael Smith <mike@mikelikescoding.com>
2016-03-18 20:12:00 -0700

    Nested OpenSCADPathFinder in OutputSettings

--- OSCADSharp/OSCADSharp/IO/OutputSettings.cs
+++ OSCADSharp/OSCADSharp/IO/OutputSettings.cs
@@ -7,12 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace OSCADSharp.IO
-{
+{   
     /// <summary>
     /// Settings for OpenSCAD scripts
     /// </summary>
     public static class OutputSettings
-    {
+    {   
         /// <summary>
         /// Code-gen header
         /// </summary>
@@ -23,6 +23,30 @@ namespace OSCADSharp.IO
         /// Path to the OpenSCAD executable for file invocation
         /// (Default value is set the default install directory on Windows)
         /// </summary>
-        public static string OpenSCADPath { get; set; } = new OpenSCADPathFinder().GetPath();   
+        public static string OpenSCADPath { get; set; } = new OpenSCADPathFinder().GetPath();
+
+        /// <summary>
+        /// Known where the default installation locations for OpenSCAD
+        /// are, and finds the right path for the current environment
+        /// </summary>
+        private class OpenSCADPathFinder
+        {
+            private string[] possibleFilePaths = new string[]
+            {
+            @"C:\Program Files (x86)\OpenSCAD\openscad.exe",
+            @"C:\Program Files\OpenSCAD\openscad.exe"
+            };
+
+            internal string GetPath()
+            {
+                foreach (string path in possibleFilePaths)
+                {
+                    if (File.Exists(path))
+                        return path;
+                }
+
+                return null;
+            }
+        }
     }
 }
